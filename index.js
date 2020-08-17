72ec4a46bc4fc0313bba41068465daf91f14607e
dnrlwo <dnrlwo11@gmail.com>
2020-08-17 22:04:42 +0900

    fix: back end index

--- index.js
+++ index.js
@@ -2,48 +2,46 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
-
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
+
 const passportConfig = require('./passport');
-
 const db = require('./models');
-
 const userAPIRouter = require('./routes/user');
-const teamAPIRouter = require('./routes/team');
-const stadiumAPIRouter = require('./routes/stadium');
+const postApiRouter = require('./routes/post');
 
 dotenv.config();
 const app = express();
-db.sequelize.sync();
+db.sequelize.sync()
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch(console.error);
+
 passportConfig();
 
-app.use(morgan('dev'));
 app.use(cors({
     origin: true,
     credentials: true,
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+})); 
+app.use(express.json()); // json형태의 data를 req.body!
+app.use(express.urlencoded({ extended: true })); // form submit시 req.body처리!
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(expressSession({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false, // https를 쓸 때 true
-    },
-    name: 'futchall',
+  saveUninitialized: false,
+  resave: false,
+  secret: process.env.COOKIE_SECRET,
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
-app.use('/api/user', userAPIRouter);
-app.use('/api/team', teamAPIRouter);
-app.use('/api/stadium', stadiumAPIRouter);
+app.use('/user', userAPIRouter);
+app.use('/post', postApiRouter);
+
+// app.use((err, req, res, next) => {
+// // 에러처리 미들웨어 custom 가능, 여기부분에서!
+// });
 
 app.listen(3065, () => {
     console.log('server is running on http://localhost:3065');
