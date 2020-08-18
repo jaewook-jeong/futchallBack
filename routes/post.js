bd8909a9f8edb320c4f550b5055464af026febec
dnrlwo <dnrlwo11@gmail.com>
2020-08-18 20:21:55 +0900

    add: comment, post routes

--- routes/post.js
+++ routes/post.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-const { Post } = require('../models');
-const { noExtendLeft } = require('sequelize/types/lib/operators');
+const { Post, Comment } = require('../models');
+const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.post('/', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.create({
       content: req.body.content,
+      UserId: req.user.id,
     });
     res.status(201).json(post);
   } catch (error) {
@@ -16,3 +17,19 @@ router.post('/', async (req, res, next) => {
     next(error);
   }
 });
+
+router.post('/:postId/comment', isLoggedIn, async (req, res, next) => {
+  try {
+    const comment = await Comment.create({
+      content: req.body,content,
+      PostId: req.params.postId,
+      UserId: req.user.id, // passport에서 deserialize 한 후 req.user에 넣어준다
+    });
+    res.status(201).json(comment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
