1a304c97158cfca682fb54bfc6aeecf793312b58
dnrlwo <dnrlwo11@gmail.com>
2020-08-28 16:34:49 +0900

    add: get stadiumlist on map

---
+++ routes/stadia.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Op } = require('sequelize');
+
+
+const db = require('../models');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const stadiumList = await db.Stadium.findAll({
+      where: {
+        lat: {
+          [Op.gte]: req.body.left,
+          [Op.lte]: req.body.right,
+        },
+        lng: {
+          [Op.gte]: req.body.bottom,
+          [Op.lte]: req.body.top,
+        }
+      },
+      include: [{
+        model: db.Image,
+      }]
+    });
+    res.status(200).json(stadiumList);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
