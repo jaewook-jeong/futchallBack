bace3215415249a6d10c01fe241101611fc46d56
dnrlwo <dnrlwo11@gmail.com>
2020-12-09 19:16:00 +0900

    test wild card

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(hpp());
   app.use(helmet());
   app.use(cors({
-    origin: 'https://futchall.com',
+    origin: '*',
     credentials: true,
   })); 
 } else {
