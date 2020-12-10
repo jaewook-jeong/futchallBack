c12590fa371a6436ddeb45614679cd0a3e58a252
dnrlwo <dnrlwo11@gmail.com>
2020-12-10 16:25:39 +0900

    fix: lambda image format

--- lambda/index.js
+++ lambda/index.js
@@ -7,20 +7,37 @@ exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name; // futchall
   const Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
   const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
-  const requiredFormat = ext;
+  const requiredFormat = Key.split('.')[Key.split('.').length - 1].toLowerCase();
   try {
-    const s3Object = await s3.getObject({ Bucket, Key }).promise();
-    const resizedImage = await sharp(s3Object.Body)
-      .resize(400, 400, { fit: 'inside' })
-      .toFormat(requiredFormat)
-      .toBuffer();
-    await s3.putObject({
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: resizedImage,
-    }).promise();
-    return callback(null, `thumb/${filename}`);
+    if (requiredFormat !== 'jpg' && requiredFormat !== 'gif') {
+      const s3Object = await s3.getObject({ Bucket, Key }).promise();
+      const resizedImage = await sharp(s3Object.Body)
+        .resize(400, 400, { fit: 'inside' })
+        .toFormat(requiredFormat)
+        .toBuffer();
+      await s3.putObject({
+        Bucket,
+        Key: `thumb/${filename}`,
+        Body: resizedImage,
+      }).promise();
+      return callback(null, `thumb/${filename}`);
+    } else if(requiredFormat === 'jpg') {
+      const s3Object = await s3.getObject({ Bucket, Key }).promise();
+      const resizedImage = await sharp(s3Object.Body)
+        .resize(400, 400, { fit: 'inside' })
+        .jpeg({
+          quality: 100,
+          force: true,
+        })
+        .toBuffer();
+      await s3.putObject({
+        Bucket,
+        Key: `thumb/${filename}`,
+        Body: resizedImage,
+      }).promise();
+      return callback(null, `thumb/${filename}`);
+    }
+    return callback(null);
   } catch (error) {
     console.error(error);
     return callback(error);
