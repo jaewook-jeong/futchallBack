712eeba5aac1a6cf511c7833e42ae2eb84af7739
dnrlwo <dnrlwo11@gmail.com>
2020-12-11 17:09:21 +0900

    fix: content allow Null

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('Post', {
     content: {
       type: DataTypes.TEXT,
-      allowNull: false,
+      allowNull: true,
     },
   },{
     charset: 'utf8mb4',
